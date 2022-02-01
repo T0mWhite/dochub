d89286787b83ce884f3599076b8251cbd1873e74
T0mWhite <tbw2.mail@gmail.com>
2022-02-01 16:29:06 -0700

    Style module added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import * as React from "react";
-import ReactDOM from "react-dom";
-import Button from "@mui/material/Button";
+// import ReactDOM from "react-dom";
+
+import SideBar from './components/SideBar';
+
 // CSS baseline reset to add consistency among different browser types.
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -8,7 +10,7 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <Button variant="contained">DocHub!</Button>
+      <SideBar />
     </>
   );
 }
