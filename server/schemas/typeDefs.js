7a9f1a92df813c3d35db39164201b0a50cfb2d31
Caswell Orr <caswell.orr@gmail.com>
2022-02-02 13:11:57 -0600

    seed, hopefully the change in the tech model goes through

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,7 +26,7 @@ const typeDefs = gql`
 
   type ContentBody {
     featureName: String!
-    featureBody: [FeatureBody]
+    featureBody: FeatureBody
     featureExample: [],
     featureReference: []
   }
