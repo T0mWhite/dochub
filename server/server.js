67699d2136b470b94f7ef7504f61979c67bda59a
Caswell Orr <caswell.orr@gmail.com>
2022-02-02 09:47:04 -0600

    server.js boiler plate

---
+++ server/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+// ========== APOLLO AND GQL ========
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
+// ========= AUTH ==========
+const { authMiddleware } = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// =========== ApolloServer ===========
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+//  =========== MIDDLEWARE ===========
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// apply midlleware to ApolloServer
+server.applyMiddleware({ app });
+
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+// =========== GET ALL ===========
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// ========== OPEN DATABASE ==========
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+
+     // log where we can go to test our GQL API
+     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  }
+)});
