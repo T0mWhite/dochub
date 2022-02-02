eb682117efe22ab1cdd4d3b90700e10c51601540
Gabe L'Herault <glherault@tcmsolutions.net>
2022-02-02 08:57:28 -0600

    navbar UI and site theme set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,40 @@
 import * as React from "react";
-import ReactDOM from "react-dom";
+import PrimarySearchAppBar from "./components/NavBar/index";
 import Button from "@mui/material/Button";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 // CSS baseline reset to add consistency among different browser types.
 import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#000000',
+      contrastText: '#1cfb3a',
+      light: '#272727',
+    },
+    secondary: {
+      main: '#00f5b5',
+      contrastText: 'rgba(226,230,86,0.87)',
+    },
+    background: {
+      default: '#000000',
+      paper: '#181818',
+    },
+    text: {
+      primary: '#00b7ff',
+    },
+  },
+});
 
 function App() {
   return (
     <>
+    <ThemeProvider theme={theme}>
+      <PrimarySearchAppBar />
       <CssBaseline />
       <Button variant="contained">DocHub!</Button>
+      </ThemeProvider>;
     </>
   );
 }
