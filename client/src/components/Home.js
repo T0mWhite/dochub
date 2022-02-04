890c21076a4723f8adc018bee478ba8151f2faa6
T0mWhite <tbw2.mail@gmail.com>
2022-02-04 12:25:17 -0700

    sign in sign up linking

---
+++ client/src/components/Home.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ReactDOM from "react-dom";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+// CSS baseline reset to add consistency among different browser types.
+import CssBaseline from "@mui/material/CssBaseline";
+
+// =========== COMPONENTS =============
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
+import CustomizedList from "./SideBar/index";
+import MainGridUi from "./MainContainer/index";
+import ResponsiveAppBar from "./Nav/index";
+// import PrimarySearchAppBar from "./NavBar/index";
+
+function Home() {
+  return (
+    <>
+      <ResponsiveAppBar position="fixed" />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={3}>
+            <CustomizedList item />
+          </Grid>
+          <Grid item xs={9}>
+            <MainGridUi />
+          </Grid>
+        </Grid>
+      </Box>
+      <CssBaseline />
+    </>
+  );
+}
+
+export default Home;
