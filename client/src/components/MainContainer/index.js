5fdd41858baed97697133383d928055086cec635
T0mWhite <tbw2.mail@gmail.com>
2022-02-09 11:44:43 -0700

    Main fails to render first try, comment out the technologyContents map and add it back in and it renders

--- client/src/components/MainContainer/index.js
+++ client/src/components/MainContainer/index.js
@@ -1,17 +1,14 @@
 import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import BeachAccessIcon from "@mui/icons-material/BeachAccess";
 import AddCircleTwoToneIcon from "@mui/icons-material/AddCircleTwoTone";
 import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import Link from '@mui/material/Link';
+import Link from "@mui/material/Link";
 import DocRating from "./Rating";
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 import Typography from "@mui/material/Typography";
-import Constex from "../../static/images/Constexample.PNG";
-import Letex from "../../static/images/Letexample.PNG";
 
 import { useQuery } from "@apollo/client";
 import { QUERY_SINGLE_TECHNOLOGY_MAIN } from "../../utils/queries";
@@ -82,14 +79,15 @@ const HoistMeCount = styled("div")(({ theme }) => ({
 }));
 
 export default function MainGridUi() {
-
-  const { technologyName } = useParams();
+  // const { technologyName } = useParams();
+  const technologyName = "JavaScript";
 
   const { loading, data } = useQuery(QUERY_SINGLE_TECHNOLOGY_MAIN, {
-    variables: { technologyName: "JavaScript" }
+    variables: { technologyName: technologyName },
   });
 
   const technology = data?.technology || {};
+  console.log(technology);
 
   return (
     <Box sx={{ flexGrow: 1, justifyContent: "center", margin: "1vw" }}>
@@ -110,103 +108,76 @@ export default function MainGridUi() {
           <DocRating />
         </Grid>
         <Grid item xs={3}>
-            <AddRevision
-              sx={{
-                border: 1,
-                borderColor: "secondary.main",
-                textAlign: "center",
-              }}
-            >
-              Add Revision
-              <AddCircleTwoToneIcon />
-            </AddRevision>
-            0 Revisions
+          <AddRevision
+            sx={{
+              border: 1,
+              borderColor: "secondary.main",
+              textAlign: "center",
+            }}
+          >
+            Add Revision
+            <AddCircleTwoToneIcon />
+          </AddRevision>
+          0 Revisions
         </Grid>
         <Grid item xs={3}>
-            <HoistMe sx={{
+          <HoistMe
+            sx={{
               border: 1,
               borderColor: "secondary.contrastText",
-            }}>
-              Hoist Me
-              <BeachAccessIcon />
-            </HoistMe>
-            <HoistMeCount
-              sx={{
-                color: "secondary.main",
-              }}
-            >
-              (34)
-            </HoistMeCount>
+            }}
+          >
+            Hoist Me
+            <BeachAccessIcon />
+          </HoistMe>
+          <HoistMeCount
+            sx={{
+              color: "secondary.main",
+            }}
+          >
+            (34)
+          </HoistMeCount>
         </Grid>
-        {/* {technology.technologyContents.map((technologyContent) => ( */}
-        <Grid item xs={12}>
-          <SubTitle>gfdgdfggr</SubTitle>
-          <Typography variant="h6" gutterBottom component="div" sx={{
-            color: 'secondary.main',
-          }}>
-            Var:</Typography>
-          <Typography variant="body1" gutterBottom>
-            Deprecated variable declaration. Not recommended for use.
-          </Typography>
-          <Typography variant="h6" gutterBottom component="div" sx={{
-            color: 'secondary.main',
-          }}>
-            Let:</Typography>
-          <Typography variant="body1" gutterBottom>
-            Standard variable declaration. Use let to create a variable that you
-            will want to change or edit. (Mutable variable)
-          </Typography>
-          <Typography variant="h6" gutterBottom component="div" sx={{
-            color: 'secondary.main',
-          }}>
-            Const:</Typography>
-          <Typography variant="body1" gutterBottom>
-            Use const(ant) to create a variable that you don't want to change.
-            (Immutable variable)
-          </Typography>
-          <Typography variant="h6" gutterBottom component="div" sx={{
-            color: 'secondary.contrastText',
-          }}>
-            Examples:
-          </Typography>
-          <Typography variant="h6" gutterBottom component="div" sx={{
-            color: 'secondary.main',
-          }}>
-            Const:
-            </Typography>
-          <img
-          src={Constex}
-          alt="ex1"
-          padding="2"
-        />
-          <Typography variant="h6" gutterBottom component="div" sx={{
-            color: 'secondary.main',
-          }}>
-            Let:
-          </Typography>
-          <img
-          src={Letex}
-          alt="ex1"
-        />
-          <Typography>
-            References:
-          </Typography>
-          <Typography>
-          <Link href="https://www.w3schools.com/jsref/jsref_var.asp" variant="body2" sx={{
-            color: 'error.main',
-          }}>
-          https://www.w3schools.com/jsref/jsref_var.asp
-          </Link>
-          </Typography>
-          <Typography>
-          <Link href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var" variant="body2" sx={{
-            color: 'error.main',
-          }}>
-          https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
-          </Link>
-          </Typography>
-        </Grid>
-        {/* ))} */}
+        {technology.technologyContents.map((technologyContent) => (
+          <Grid item xs={12}>
+            <SubTitle>{technologyContent.contentTitle}</SubTitle>
+            {technologyContent.contentBody.map((content) => (
+              <>
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  component="div"
+                  sx={{
+                    color: "secondary.main",
+                  }}
+                >
+                  {content.featureName}:
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  {content.featureBody}
+                </Typography>
+                <Typography>Examples:</Typography>
+                {content.featureExample.map((example) => (
+                  <Typography>{example}</Typography>
+                ))}
+                <Typography>References:</Typography>
+                {content.featureReference.map((reference) => (
+                  <Typography>
+                    <Link
+                      href={reference}
+                      variant="body2"
+                      sx={{
+                        color: "error.main",
+                      }}
+                    >
+                      {reference}
+                    </Link>
+                  </Typography>
+                ))}
+              </>
+            ))}
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
